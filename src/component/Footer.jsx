fd8ff591ce42a84776d555a577f2bed665b5a7b2
Abhish1sb <abhish1albodi@gmail.com>
2022-09-12 21:43:03 +0530

    adding sweet alert on succesfull deletion

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const year=new Date().getFullYear();
-// console.log(year);
+
 function Footer(){
     return(
-        <footer><p>Copyright @ {year}</p></footer>
+    <div>
+    <footer><p>Copyright @ {year}</p></footer>
+    </div>
     );
 }
 export default Footer;
--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Swal from 'sweetalert2'
 
 function Note({ id, title, content, notes, setNotes }){
     const deleteNote = (id) => {
+        Swal.fire('The note was deleted')
         setNotes(
           notes.filter((item, index) => {
             return index !== id ? item : null;
